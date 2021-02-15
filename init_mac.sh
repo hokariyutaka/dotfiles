5fc1b0655061cfca32195e07be1f6ea0b79cf5ee
hokariyutaka <hokari.yutaka@gmail.com>
2021-02-15 18:28:52 +0900

    Create init_mac.sh

---
+++ init_mac.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/exec bash
+#
+# Usage: init_mac.sh
+#
+
+# exec option
+# set -eux
+
+# get administrator password
+sudo -v
+
+# change hostname
+echo ""
+echo "Would you like to set your computer name?  (y/n)"
+read -r response
+if [[ $response =~ ([yY]) ]] ; then
+  echo "What name would you like?"
+  read HOST_NAME
+  sudo scutil --set ComputerName $HOST_NAME
+  sudo scutil --set LocalHostName $HOST_NAME
+fi
+
+# disable shadow when screencapture
+defaults write com.apple.screencapture disable-shadow -boolean true
+
+# change screencapture location
+SCREENSHOT_TARGET=~/Pictures/ScreenShots
+if [ ! -d $SCREENSHOT_TARGET ]; then
+  mkdir -p $SCREENSHOT_TARGET
+fi
+defaults write com.apple.screencapture location $SCREENSHOT_TARGET
